a9a321caf216c6f45ae61c17862dc3374b3968ac
Luiz Carlos <luiz.ramos@animaeducacao.com.br>
2022-02-07 22:26:36 -0300

    Envio final Login

--- endereco.js
+++ endereco.js
@@ -15,23 +15,28 @@ describe("Desafio - NopCommerce", () => {
     const valcar = new Validação();
     const concluir = new Finalização();
     const qtd = 4;
+    const email = "qatech138@gmail.com"
+    const senha = "12345678"
+    const pgto = 2
 
     it("Login", () => {
         site.acessoSite();
         site.botaoRegistro();
-        site.registro();
+        site.registro(email, senha);
         site.validaRegistro();
         site.botaoProsseguir();
         checkout.botaoLogout();
     })
     it("Tela de produtos e checkout", () => {
         site.acessoSite();
-        site.botaoLogin();
+        site.botaoLogin(email, senha);
         item.buscar(qtd);
         valcar.botaoCart();
         valcar.validaCart(qtd);
         concluir.conclusao();
         checkout.teladecheckout();
+        checkout.formadepagamento(pgto);
+        checkout.validacaoCompra();
         checkout.botaoLogout();
     })
 });
