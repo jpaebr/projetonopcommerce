f77074b1b93934f186f4dd9c7508ea7a0e6094c0
João Pedro <joaopedrodiassantos@gmail.com>
2022-02-07 22:34:49 -0300

    Envio arquivo final de checkout

--- enderecoclasses.js
+++ enderecoclasses.js
@@ -8,9 +8,28 @@ export class PaginaCheckout {
         cy.get('#BillingNewAddress_PhoneNumber').type("75999547841");
         cy.get('#billing-buttons-container > .new-address-next-step-button').click();
         cy.get('#shipping-method-buttons-container > .button-1').click();
-        cy.get('#payment-method-buttons-container > .button-1').click();
-        cy.get('#payment-info-buttons-container > .button-1').click();
-        cy.get('#confirm-order-buttons-container > .button-1').click();
+    }
+    formadepagamento(pgto) {
+        switch (pgto) {
+            case 1:
+                cy.get('#payment-method-buttons-container > .button-1').click();
+                cy.get('#payment-info-buttons-container > .button-1').click();
+                cy.get('#confirm-order-buttons-container > .button-1').click();
+                break;
+            default:
+                cy.get('#paymentmethod_1').click();
+                cy.get('#payment-method-buttons-container > .button-1').click();
+                cy.get('#CreditCardType').select("Master card");
+                cy.get('#CardholderName').type("QA TECH");
+                cy.get('#CardNumber').type("8986545678990900000009");
+                cy.get('#ExpireMonth').select("06");
+                cy.get('#ExpireYear').select("2026");
+                cy.get('#CardCode').type("514");
+                cy.get('#payment-info-buttons-container > .button-1').click();
+                cy.get('#confirm-order-buttons-container > .button-1').click();
+        }
+    }
+    validacaoCompra() {
         cy.get('.section > .title > strong').should("contain", "Your order has been successfully processed!");
     }
     botaoLogout() {
